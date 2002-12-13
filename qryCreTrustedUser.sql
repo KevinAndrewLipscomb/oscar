f0bffabeedb5c0cf72b75c23c59992bff8ee2d3e
Kevin Andrew Lipscomb <kevinandrewlipscomb@vt.edu>
2002-12-13 20:52:34 +0000

    Updated to be consistent with structure of actual production table.

--- qryCreTrustedUser.sql
+++ qryCreTrustedUser.sql
@@ -1,25 +1,31 @@
+#
+# $Id#
+#
+
 # phpMyAdmin MySQL-Dump
-# version 2.2.0rc1
+# version 2.2.4
 # http://phpwizard.net/phpMyAdmin/
 # http://phpmyadmin.sourceforge.net/ (download page)
 #
 # Host: localhost
-# Generation Time: October 18, 2001, 1:55 pm
-# Server version: 3.23.32
-# PHP Version: 4.0.6
-# Database : oscar
+# Generation Time: Dec 13, 2002 at 03:50 PM
+# Server version: 3.23.46
+# PHP Version: 4.2.3
+# Database : `oscar`
 # --------------------------------------------------------
 
 #
-# Table structure for tables 'trusted_user' and 'x_trusted_user'
+# Table structure for tables `trusted_user` and `x_trusted_user`
 #
 
-CREATE TABLE `trusted_user` (
-  `email_address` varchar(64) NOT NULL default '',
-  PRIMARY KEY (`email_address`)
+CREATE TABLE trusted_user (
+  email_address varchar(64) NOT NULL default '',
+  coord_agency enum('EMS','R01','R02','R04','R05','R06','R09','R13','R14','R15','R16','R17') NOT NULL default 'EMS',
+  PRIMARY KEY  (email_address,coord_agency)
 ) TYPE=MyISAM;
 
-CREATE TABLE `x_trusted_user` (
-  `email_address` varchar(64) NOT NULL default '',
-  PRIMARY KEY (`email_address`)
+CREATE TABLE x_trusted_user (
+  email_address varchar(64) NOT NULL default '',
+  coord_agency enum('EMS','R01','R02','R04','R05','R06','R09','R13','R14','R15','R16','R17') NOT NULL default 'EMS',
+  PRIMARY KEY  (email_address,coord_agency)
 ) TYPE=MyISAM;
