2f1db5d3ce5f0274abe4ea0adc2bbe306a5e9118
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2002-12-13 21:07:40 +0000

    Fixed bug # 653448, which was preventing the Trusted User feature from operating correctly.

--- trunk/oscar/qryCreTrustedUser.sql
+++ trunk/oscar/qryCreTrustedUser.sql
@@ -8,24 +8,29 @@
 # http://phpmyadmin.sourceforge.net/ (download page)
 #
 # Host: localhost
-# Generation Time: Dec 13, 2002 at 03:50 PM
+# Generation Time: Dec 13, 2002 at 04:04 PM
 # Server version: 3.23.46
 # PHP Version: 4.2.3
 # Database : `oscar`
 # --------------------------------------------------------
 
 #
-# Table structure for tables `trusted_user` and `x_trusted_user`
+# Table structure for table `trusted_user`
 #
 
 CREATE TABLE trusted_user (
   email_address varchar(64) NOT NULL default '',
-  coord_agency enum('EMS','R01','R02','R04','R05','R06','R09','R13','R14','R15','R16','R17') NOT NULL default 'EMS',
+  coord_agency enum('EMS','Rescue1','Rescue2','Rescue4','Rescue5','Rescue6','Rescue9','Rescue13','Rescue14','Rescue15','Rescue16','Rescue17','DiveTeam') NOT NULL default 'EMS',
   PRIMARY KEY  (email_address,coord_agency)
 ) TYPE=MyISAM;
+# --------------------------------------------------------
+
+#
+# Table structure for table `x_trusted_user`
+#
 
 CREATE TABLE x_trusted_user (
   email_address varchar(64) NOT NULL default '',
-  coord_agency enum('EMS','R01','R02','R04','R05','R06','R09','R13','R14','R15','R16','R17') NOT NULL default 'EMS',
+  coord_agency enum('EMS','Rescue1','Rescue2','Rescue4','Rescue5','Rescue6','Rescue9','Rescue13','Rescue14','Rescue15','Rescue16','Rescue17','DiveTeam') NOT NULL default 'EMS',
   PRIMARY KEY  (email_address,coord_agency)
 ) TYPE=MyISAM;
