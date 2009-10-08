83690e0908fffd80d906c832bc585a9708ad7fca
KevinAnLipscomb <KevinAnLipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2009-10-08 20:21:25 +0000

    Added support for Air Ambulance team.  Updated indicators to reflect EMS-7 program.  Switched random photo display from cats to eye-candy.  Improved pre-production testing arrangement.

---
+++ trunk/oscar/0-render-safe-for-d-and-x.sql
@@ -0,0 +1,4 @@
+update ignore authority set email_addr = concat(email_addr,"@frompaper2web.com");
+update ignore avail_sheet set email_addr = concat(first_name,last_name,"@frompaper2web.com");
+update ignore daily_report_recipient set email_address = concat(email_address,"@frompaper2web.com");
+update ignore trusted_user set email_address = concat(email_address,"@frompaper2web.com");
