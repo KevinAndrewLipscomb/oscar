0ef2b0f63fd5bb6926e86fe068685d3d098e53b9
Kevin Andrew Lipscomb <kevinandrewlipscomb@vt.edu>
2024-02-26 17:55:38 -0500

    Converted work script from CMD to PS1.  Converted 0-REFRESH from CMD to PS1.

--- work.bat
+++
@@ -1,7 +0,0 @@
-REM
-REM $Id: work.bat 7035 2020-04-13 18:47:08Z kevinanlipscomb $
-REM
-cd "C:\Inetpub\wwwroot\oscar"
-start /max explorer /e,/select,C:\Inetpub\wwwroot\oscar\.git
-start /max oscar.sln
-IF EXIST "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe" (start "" /max "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe") ELSE start "" /max "C:\Program Files (x86)\MySQL\MySQL Workbench\MySQLWorkbench.exe"
